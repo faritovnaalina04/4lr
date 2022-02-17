42e7539462e377bb430f0874a9f1993d46c32c42
alinashaymardanova <98107093+alinashaymardanova@users.noreply.github.com>
2022-02-17 17:53:43 +0300

    alina

--- main.py
+++ main.py
@@ -17,7 +17,19 @@ def dictionary(n):
     return result
 
 def duplicate(a: dict):
-    pass
+keys = list(a.keys())
+dubl = {}
+for i1 in range(len(keys) - 1):
+key1 = keys[i1]
+result_key = (a[key1], key1)
+for i2 in range(i1 +1, len(keys)):
+key2 = keys[i2]
+if path.basename(key1) == path.basename(key2) and a[key1] == a[key2]:
+if result_key not in dubl:
+dubl[result_key] = []
+dubl[result_key].append(key2)
+if result_key in dubl:
+yield result_key, dubl[result_key]
 
 def duplicate_2(result):
     pass
